c48251c3d59037f2854b7568d075f61ac634eaa8
oong26 <mkhalil26122000@gmail.com>
2022-01-19 14:31:41 +0700

    Benerin error list invoice.

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -450,9 +450,12 @@ class TransaksiController extends Controller
                         ->join('pembayaran as p', 'p.kode_transaksi', 'transaksi.kode_transaksi')
                         ->where('transaksi.status_bayar', 'Belum')
                         ->whereBetween('p.waktu', ["$dari 00:00:00", "$sampai 23:59:59"]);
-        }
 
-        return \view('transaksi.invoice.list-all-invoice', ['transaksi' => $transaksi->paginate(10)], $this->param);
+                        return \view('transaksi.invoice.list-all-invoice', ['transaksi' => $transaksi->paginate(10)], $this->param);
+        }
+        else {
+            return \view('transaksi.invoice.list-all-invoice',  $this->param);
+        }
     }
 
     public function paid($kode)
